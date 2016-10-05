603a7ee85fe9256cc5ebf8e0205acd2a8c3d9491
andar9 <andylitalo@yahoo.com>
2016-10-05 09:58:21 -0700

    October 2016
    Version found on spin-coater computer 10/4/16

--- reduce_data.py
+++ reduce_data.py
@@ -5,41 +5,47 @@ from os import path
 import matplotlib.pyplot as plt
 import numpy as np
 import cPickle as pkl
+import cv2
 
 # Custom modules
 import Functions as Fun
+import VideoFunctions as VF
 import ImageProcessingFunctions as IPF
 
 # User parameters
-folder = '..\\Data\\Demo\\'
-fileString = '*\\*_data.pkl'
-saveName = folder + 'all_data1.pkl'
+folder = '..\\Data\\Cole\\220616\\temp\\'
+fileString = '*_data1.pkl'
+saveName = '../Data/Cole/220616/temp/Data.pkl'
 step = 1
+viewResults = True
 diskScale = 15 # radius in [cm]
-restrictAngle = False
-t0 = 0
-t1 = 90
-stride = 10
+#QList = [500,1000,1500,2000,2500,3000]#,3250,3500]
+#RPMList = [0]#,30,120,250,500,750,1000]
 
-# Parameters for testing
-viewResults = False
+allData = {}
 
+   
 # Cleanup existing windows
 plt.close('all')
     
-# Get list of video files to process and initialize data dictionary
+# Get list of video files to process
 pathToFiles = path.join(folder,fileString)
 fileList = glob.glob(pathToFiles)
 nFiles = len(fileList)
-allData = {}
 
 for i in range(0,nFiles):
-    # Use Try/Catch to skip bad files without stopping analysis
     try:
-        # Parse the filename to get video info and load data container
+        # Parse the filename to get video info
         dataPath = fileList[i]
         keyName = path.split(dataPath)[1][:-9]
-        condition = path.split(path.split(dataPath)[0])[1]
+        
+        
+        RPM = VF.get_RPM_from_file_name(dataPath)
+        Q = VF.get_flowRate_from_file_name(dataPath)
+        
+        
+        
+        # Load data
         with open(dataPath,'rb') as f:
             container = pkl.load(f)
         
@@ -47,27 +53,22 @@ for i in range(0,nFiles):
         dataFrames = sorted(container['data'].keys())
         N = len(dataFrames)
         center = container['maskData']['diskCenter']
-        pix_per_cm = container['maskData']['diskRadius']/diskScale
-        maskRadius = container['maskData']['maskRadius']/pix_per_cm # [cm]
+        diskRadius = container['maskData']['diskRadius']
+        maskRadius = container['maskData']['maskRadius']
         fps = container['fps']
-        RPM = container['RPM']
         t0Frame = container['t0Frame']
         flowRate = container['flowRate']
-        nozzleMask = container['maskData']['nozzleMask']      
+        nozzleMask = container['maskData']['nozzleMask']
+        kernel = np.ones((5,5),np.uint8)
+        dilation = cv2.erode(np.uint8(nozzleMask),kernel,iterations = 1)
+        nozzleMask = (dilation==1)    
         
-        # Operate on the nozzle mask so that it will remove contour around it
-        dilatedMask = IPF.dilate_mask(nozzleMask)
-        if RPM > 0: # Convert to 8 bit image for rotation
-            dilatedMask = dilatedMask*np.uint8(255)
-        else:
-            mask = dilatedMask
-        
-        # Initialize variables to store in reduced data
-        time = np.zeros(N)
+        # Initialize variables to plot
         aMax = np.zeros(N)
         aMin = np.zeros(N)
         aMean = np.zeros(N)
         aMax2 = np.zeros(N)
+        time = np.zeros(N)
         aMin2 = np.zeros(N)
         area = np.zeros(N)
         perimeter = np.zeros(N)
@@ -76,147 +77,76 @@ for i in range(0,nFiles):
         
         # Loop through frames in the video and track wetted area
         for j in range(0,N,step):
+            
+            # Load data
+            num = dataFrames[j]
+            time[j] = num/fps
+            contour = np.float32(container['data'][num])
+            
+            if len(contour) <= 2:
+                continue
+            temp = nozzleMask[np.int16(contour[:,1]),np.int16(contour[:,0])]
+            contour = contour[temp]
+            if len(contour) < 2:
+                continue
+            # Remove points along the nozzle mask
+            xData = contour[:,0]
+            yData = contour[:,1]
+            diffX = xData[1:]-xData[:-1]
+            diffY = yData[1:]-yData[:-1]
+            if max(diffX) > max(diffY):
+                ind = np.argmax(diffX) + 1
+            else:
+                ind = np.argmax(diffY) + 1
+            x0 = np.array([center[0]])
+            y0 = np.array([center[1]])
+            xData = np.concatenate((x0,xData[ind:],xData[:ind],x0))
+            yData = np.concatenate((y0,yData[ind:],yData[:ind],y0))
+            points = [(xData[i],yData[i]) for i in range(len(xData))]
+            temp = IPF.create_polygon_mask(nozzleMask,points)[0]
+            area[j] = np.sum(temp)*(diskScale/diskRadius)**2
+            
+            # Shift data relative to center of disk and scale
+            xData = (xData - center[0])/diskRadius*diskScale
+            yData = (-yData + center[1])/diskRadius*diskScale
+            # Compute radius, and angle of each point
+            phi = np.arctan2(yData,xData)
+            phi[phi<0] += 2*np.pi
+            a = np.sqrt(xData**2 + yData**2)
+            a[a>maskRadius/diskRadius*diskScale] = maskRadius/diskRadius*diskScale
+    
+            temp = phi[-2]-phi[1]
+            if temp < 0:
+                dTheta[j] = 2*np.pi + temp
+            else:
+                dTheta[j] = temp
+            aMax[j] = np.max(a)
+            aMin[j] = np.min(a)
+            aMean[j] = np.mean(a)
+            a98 = np.percentile(a,98)
+            a02 = np.percentile(a,2)
+            aMax2[j] = np.mean(a[a>=a98])
+            aMin2[j] = np.mean(a[a<=a02])
+            # Compute excess perimeter
+            stride = 10
+            x1 = xData[1:-1:stride]
+            y1 = yData[1:-1:stride]
+            ds = np.sqrt((x1[1:]-x1[:-1])**2 + (y1[1:]-y1[:-1])**2)
+            perimeter[j] = np.sum(ds)
         
-            try: # Use Try/Catch to skip bad frames
-                # Load data 
-                num = dataFrames[j]
-                contour1 = np.float32(container['data'][num])
-                if len(contour1) <= 2: # Skip frames without enough points
-                    continue
-                theta = container['theta'][num]%(-360)
-                
-                # Adjust the rotation of the mask to match the stored data
-                if RPM > 0:
-                    mask = IPF.rotate_image(dilatedMask,theta,
-                                            size=np.shape(nozzleMask))
-                    mask = (mask == 255)
-                    
-                # Remove contour points that are covered by the mask
-                temp = mask[np.int16(contour1[:,1]),np.int16(contour1[:,0])]
-                ind0 = np.argmax(temp == False)
-                ind1 = len(temp) - np.argmax(temp[::-1] == False) - 1
-                # Reorder temp if mask is covering beginning or end of temp
-                if ((ind0 == 0) or (ind1 == len(temp)-1)) or (ind1-ind0>0.75*len(temp)):
-                    halfInd = len(temp)/2
-                    temp = np.concatenate((temp[halfInd:],temp[:halfInd]))
-                    ind0 = np.argmax(temp == False)
-                    ind1 = len(temp) - np.argmax(temp[::-1] == False) - 1
-                    temp[ind0:ind1] = False
-                    midInd = len(temp)-halfInd
-                    temp = np.concatenate((temp[midInd:],temp[:midInd]))
-                else:
-                    temp[ind0:ind1] = False
-                contour = contour1[temp]
-                if len(contour) < 2:
-                    continue
-                # Convert to x and y values and reorder so that the first 
-                # point starts at the right edge of the nozzle and goes
-                # counter-clockwise along the wave front
-                xData = contour[:,0]
-                yData = contour[:,1]
-                diffX = xData[1:]-xData[:-1]
-                diffY = yData[1:]-yData[:-1]
-                diffD = np.sqrt(diffX**2 + diffY**2)
-                if np.max(diffD) > 3:
-                    ind = np.argmax(diffD) + 1
-                    xData = np.concatenate((xData[ind:],xData[:ind]))
-                    yData = np.concatenate((yData[ind:],yData[:ind]))
-                    
-                if restrictAngle:
-                    # Compute the angle of each point relative to disk center
-                    xData1 = (xData - center[0])
-                    yData1 = (-yData + center[1])
-                    phi = np.arctan2(yData1,xData1)
-                    phi[phi<0] += 2*np.pi
-                    # Shift the indices so that it starts at the last point 
-                    # where the angle crosses zero
-                    diffPhi = phi[1:] - phi[:-1]
-                    ind = len(diffPhi) - np.argmax(diffPhi[::-1]<-np.pi)
-                    phi = np.concatenate((phi[ind:],phi[:ind]))
-                    xData  = np.concatenate((xData[ind:],xData[:ind]))
-                    yData  = np.concatenate((yData[ind:],yData[:ind]))
-                    # Remove wrap around values from phi
-                    diffPhi = phi[1:] - phi[:-1]
-                    temp = np.concatenate(([False],(diffPhi < -np.pi)))
-                    phi[temp] += 2*np.pi
-                    # Rotate the cutoff angles and find corresponding indices
-                    tStart = (t0 + theta)/180*np.pi
-                    if tStart < 0:
-                        tStart += 2*np.pi
-                    tEnd = (t1 + theta)/180*np.pi
-                    if tEnd < 0: 
-                        tEnd += 2* np.pi
-                    ind0 = np.argmax(phi>tStart)
-                    ind1 = len(phi) - np.argmax(phi[::-1]<tEnd) - 1
-                    # Remove points outside the specified angle range
-                    if ind1>ind0:
-                        xData = xData[ind0:ind1]
-                        yData = yData[ind0:ind1]
-                    else:
-                        xData = np.concatenate((xData[ind0:],xData[:ind1]))
-                        yData = np.concatenate((yData[ind0:],yData[:ind1]))
-                    if len(xData) < 2*stride:
-                        continue
-                    
-                # Produce a mask of the effective wetted area
-                x0 = np.array([center[0]])
-                y0 = np.array([center[1]])
-                xData1 = np.concatenate((x0,xData[ind:],xData[:ind],x0))
-                yData1 = np.concatenate((y0,yData[ind:],yData[:ind],y0))
-                points = [(xData1[z],yData1[z]) for z in range(len(xData1))]
-                wettedArea = IPF.create_polygon_mask(mask,points)[0]
-                wettedArea = np.sum(wettedArea)/(pix_per_cm**2)
-                
-                # Shift data relative to center of disk and scale
-                xData = (xData - center[0])/pix_per_cm
-                yData = (-yData + center[1])/pix_per_cm
-                        
-                if viewResults:
-                    plt.figure(66)
-                    plt.cla()
-                    plt.plot(xData,yData,'k.-')
-                    plt.plot(xData[0],yData[0],'g.',xData[-1],yData[-1],'r.')
-                    Fun.plot_circles(diskScale,(0,0),1)
-                    Fun.plot_circles(maskRadius,(0,0),1)
-                    plt.axis('scaled')
-                    plt.pause(0.001)
-                    
-                # Compute radius, and angle of each point
-                phi = np.arctan2(yData,xData)
-                phi[phi<0] += 2*np.pi
-                a = np.sqrt(xData**2 + yData**2)
-                # Compute the range of angles analyzed on disk
-                dPhi = phi[-1]-phi[0]
-                if dPhi < 0:
-                    dPhi += 2*np.pi
-                # Compute contour length of points
-                x1 = xData[::stride]
-                y1 = yData[::stride]
-                ds = np.sqrt((x1[1:]-x1[:-1])**2 + (y1[1:]-y1[:-1])**2)
-                length = np.sum(ds)
-                
-                
-                # Store all reduced variables of interest
-                time[j] = num/fps
-                area[j] = wettedArea
-                dTheta[j] = dPhi
-                aMax[j] = np.max(a)
-                aMin[j] = np.min(a)
-                aMean[j] = np.mean(a)
-                aMax2[j] = np.percentile(a,98)
-                aMin2[j] = np.percentile(a,2)
-                perimeter[j] = length
-                
-            except:
-                print 'Analysis failed on frame number %i' %j
+            if viewResults:
+                plt.figure(66)
+                plt.cla()
+                plt.plot(xData,yData,'k.-')
+                plt.plot(xData[1],yData[1],'g.',xData[-2],yData[-2],'r.')
+                Fun.plot_circles(diskScale,(0,0),1)
+                Fun.plot_circles(maskRadius/diskRadius*diskScale,(0,0),1)
+                plt.axis('scaled')
+                plt.pause(0.001)
              
-        # Compute the excess perimeter
-        eP = perimeter/(dTheta*np.sqrt(area/np.pi))
-        # store reduced data in dictionary
+        # store data
         data = {}
-        data['dilatedMask'] = dilatedMask
-        data['stride'] = stride
-        data['condition'] = condition
+        data['condition'] = path.split(path.split(dataPath)[0])[1]
         data['flowRate'] = flowRate
         data['RPM'] = RPM
         data['fps'] = fps
@@ -230,12 +160,16 @@ for i in range(0,nFiles):
         data['area'] = area[time>0]
         data['dTheta'] = dTheta[time>0]
         data['perimeter'] = perimeter[time>0]
-        data['excessPerimeter'] = eP[time>0]
+        data['excessPerimeter'] = perimeter[time>0]/np.sqrt(2*area[time>0]* \
+                                        dTheta[time>0])
         allData[keyName] = data
-        print 'Processed %s; file %i of %i' %(keyName,i+1,nFiles)
-
+        print 'Processed %s; file %i of %i' %(keyName,i,nFiles)
+        
+        plt.figure()
+        plt.plot(time,perimeter/np.sqrt(2*area*dTheta))
+        plt.pause(0.001)
     except:
-        print 'Error on %s' %keyName
+        continue
     
 with open(saveName,'wb') as f:
     pkl.dump(allData,f)
